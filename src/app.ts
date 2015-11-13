fc0419e87a445b8ae35b361d2b99c8944f4a317d
angular-cli <angular-cli@angular.io>
2015-11-13 23:00:59 +0100

    chore: initial commit from angular-cli v0.0.0

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/angular2';
+import {LeapApp} from './app/leap';
+
+
+bootstrap(LeapApp);
---
+++ src/app/leap.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'leap-app',
+  providers: [],
+  templateUrl: 'app/leap.html',
+  directives: [],
+  pipes: []
+})
+export class LeapApp {
+
+  constructor() {}
+
+}
