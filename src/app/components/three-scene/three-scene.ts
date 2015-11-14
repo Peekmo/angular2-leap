dc2d3539daba569343378c90bd4bb16b5c0df93b
Peekmo <axel.anceau@gmail.com>
2015-11-14 21:48:50 +0100

    Transparent background

--- src/app/components/three-scene/three-scene.ts
+++ src/app/components/three-scene/three-scene.ts
@@ -17,7 +17,7 @@ export class ThreeScene {
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 3000 );
 
-    this.renderer = new THREE.WebGLRenderer();
+    this.renderer = new THREE.WebGLRenderer({alpha: true});
     this.renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild(this.renderer.domElement);
 
